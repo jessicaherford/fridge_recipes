177c56cae133c458d89cc8699ab8ee16ab2be00c
JessicaHerford <jrherford12@gmail.com>
2015-12-17 20:16:41 -0700

    Almost Done

--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,15 @@
-if(window.sessionstorage){
-  //gets form element
-  var txtUsername = document.getElementById('username');
 
-  //Element populated by session Storage
-  txtUsername.value = sessionStorage.getItem('username');
-
-
-  //saves data
-  txtUsername.addEventListener('input', function(){
-    sessionStorage.setItem('username', txtUsername.value);
-  }, false);
-
-}
+// if(window.sessionstorage){
+//   //gets form element
+//   var txtUsername = document.getElementById('username');
+//
+//   //Element populated by session Storage
+//   txtUsername.value = sessionStorage.getItem('username');
+//
+//
+//   //saves data
+//   txtUsername.addEventListener('input', function(){
+//     sessionStorage.setItem('username', txtUsername.value);
+//   }, false);
+//
+// }
